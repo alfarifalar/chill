fcc98f620726e7b42b44aa31e968929013dfd4c7
sega <sshunter2013@gmail.com>
2026-03-19 13:55:22 +0700

    feat: set auth layout with bg map

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation} from 'react-router-dom'
 
 function AuthLayout() {
+  const location = useLocation();
+  const path = location.pathname.split("/").pop();
+
+  const bgMap = {
+    login: "login.webp",
+    register: "register.webp",
+  };
+
+const bg = bgMap[path];
+
   return (
-    <main className=''>
+    <main 
+      className="h-screen box-border bg-cover bg-center bg-no-repeat bg-black/30 text-light"
+      style={{ backgroundImage: `url('/img/${bg}')` }}
+    >
       <Outlet/>
     </main>
   )
